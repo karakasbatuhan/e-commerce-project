0b980c943ee9e1d3884040c459ece3e83d66c828
Batuhan Karakaş <karakasbatuhan96@gmail.com>
2025-10-17 03:09:05 +0300

    Update 3
    -Slider bitti.
    -Footer eklendi.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,12 +9,13 @@ import { User } from "lucide-react";
 import { Search } from "lucide-react";
 import { ShoppingCart } from "lucide-react";
 import { Heart } from "lucide-react";
+import { BsChevronDown } from 'react-icons/bs';
 
 export default function Header() {
   return (
     <div className="font-montserrat">
     <div className="bg-[#252B42] flex justify-between text-white p-3 w-full font-medium">
-      <div className="ml-10 flex gap-10 text-sm">
+      <div className="ml-6 flex gap-10 text-sm">
         <h6 className="flex gap-2 items-center text-[12px]"><Phone size={15}/> (225) 555-0118</h6>
         <a href="" className="flex gap-2 items-center text-[12px]"><Mail size={15}/> michelle.rivera@example.com</a>
       </div>
@@ -29,42 +30,42 @@ export default function Header() {
     </div>
     <div className="flex p-4"> 
       <h3 className="text-2xl font-bold ml-5">Bandage</h3>
-      <div className="flex w-full overflow-hidden justify-between items-center ml-20">
+      <div className="flex w-full overflow-hidden justify-between items-center ml-18">
         <nav>
-          <ul className="flex">
-            <li className="mr-4"><a href="" className="font-bold text-[#737373] hover:text-black text-sm">Home</a></li>
+          <ul className="flex ml-10">
+            <li className="mr-4"><a href="" className="font-bold text-[#737373] hover:text-black text-sm transition-all duration-300">Home</a></li>
             <div className="group">
-              <li className="mr-4"><a href="" className="font-bold text-[#737373] hover:text-black text-sm">Shop</a></li>
+              <li className="mr-4"><a href="" className="font-bold text-[#737373] hover:text-black text-sm transition-all duration-300 justify-between items-center">Shop <BsChevronDown size={12} className="inline-block ml-1" /></a></li>
               <div className="scale-y-0 group-hover:scale-y-100 origin-top duration-300 absolute transition-all bg-white text-black p-4 flex flex-row gap-29">
                 <div className="flex flex-col">
                   <a href="" className="mb-10 font-bold text-sm">Kadın</a>
-                  <a href="" className="font-bold text-gray-500 hover:text-black text-sm">Bags</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Belts</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Cosmetics</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Bags</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Hats</a>
+                  <a href="" className="font-bold text-gray-500 hover:text-black text-sm transition-all duration-300">Bags</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Belts</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Cosmetics</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Bags</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Hats</a>
                 </div>
                 <div className="flex flex-col mr-27">
                   <a href="" className="mb-10 font-bold text-sm">Erkek</a>
-                  <a href="" className="font-bold text-gray-500 hover:text-black text-sm">Bags</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Belts</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Cosmetics</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Bags</a>
-                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm">Hats</a>
+                  <a href="" className="font-bold text-gray-500 hover:text-black text-sm transition-all duration-300">Bags</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Belts</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Cosmetics</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Bags</a>
+                  <a href="" className="font-bold text-gray-500 mt-5 hover:text-black text-sm transition-all duration-300">Hats</a>
                 </div>
               </div>
             </div>
-            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm">About</a></li>
-            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm">Blog</a></li>
-            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm">Contact</a></li>
-            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm">Pages</a></li>
+            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm transition-all duration-300">About</a></li>
+            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm transition-all duration-300">Blog</a></li>
+            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm transition-all duration-300">Contact</a></li>
+            <li className="mr-4"><a href="" className="font-bold text-[#737373]  hover:text-black text-sm transition-all duration-300">Pages</a></li>
           </ul>
         </nav>
         <div className="flex text-[#23A6F0] mr-8">
           <a href="" className="mr-6 flex items-center gap-1 font-bold text-sm"><User size={18} className="mb-1"/> Login / Register</a>
-          <a href="" className="mr-6 flex items-center"><Search size={18}/></a>
-          <a href="" className="mr-6 flex items-center"><ShoppingCart size={18}/></a>
-          <a href="" className="mr-6 flex items-center"><Heart size={18}/></a>
+          <a href="" className="mr-6 flex items-center hover:scale-120 transition-all duration-300"><Search size={18}/></a>
+          <a href="" className="mr-6 flex items-center hover:scale-120 transition-all duration-300"><ShoppingCart size={18}/></a>
+          <a href="" className="mr-5 flex items-center hover:scale-120 transition-all duration-300"><Heart size={18}/></a>
         </div>
       </div>
     </div>
--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -1,15 +1,21 @@
+import {BsChevronLeft, BsChevronRight} from "react-icons/bs"
+
 export default function HeroSlider() {
   return (
-    <section>
-      <div className="bg-blue-500 w-full h-[716px] flex justify-between items-center text-white">
-        <button>Left</button>
-        <div className="flex flex-col gap-6 text-white max-w-lg border-2 border-red-500">
-          <p>Summer 2020</p>
-          <h1>New Collection</h1>
-          <p>We know how large objects will act, but thing on a small scale.</p>
-          <button>Shop Now</button>
+    <section className="bg-[url(./assets/slider-bg.jpg)] w-full h-[44.8rem]  bg-no-repeat bg-cover font-montserrat flex flex-col justify-center items-center">
+      <div className="w-full flex justify-between items-center text-white mt-35">
+        <BsChevronLeft size={60} className="btn-prev ml-5 cursor-pointer flex hover:scale-130 transition-all duration-300"/>
+        <div className="flex flex-col justify-center gap-6 text-white max-w mr-230 mt-17">
+          <h5 className="font-bold text-base">SUMMER 2020</h5>
+          <h1 className="font-bold text-[3.63rem] leading-20 whitespace-nowrap">NEW COLLECTION</h1>
+          <h4 className="font-medium text-[1.25rem] w-[23.5rem]">We know how large objects will act, but thing on a small scale.</h4>
+          <button className="text-white bg-[#2DC071] w-[13.8rem] h-[3.88rem] font-bold text-2xl hover:bg-[#2DAE61]  transition-all duration-500">SHOP NOW</button>
         </div>
-        <button>Right</button>
+        <BsChevronRight size={60} className="btn-next mr-5 cursor-pointer flex hover:scale-130 transition-all duration-300"/>       
+      </div>
+      <div className="flex gap-0.5 mt-35">
+        <button className="w-15.5 h-2.5 bg-white cursor-pointer" aria-current="true"></button>
+        <button className="w-15.5 h-2.5 bg-white opacity-60 cursor-pointer" aria-current="true"></button>        
       </div>
     </section>
   );
